d09d670fe2ae7a2f695a49eea64776499eddc9b4
xiaotiejiang888 <605113068@qq.com>
2016-03-21 23:34:47 +0800

    用maven管理struts项目

---
+++ src/main/java/cn/sxt/action/HelloAction.java
@@ -0,0 +1,31 @@
+package cn.sxt.action;
+
+import com.opensymphony.xwork2.Action;
+
+
+/**
+ * Created by Administrator on 2016/3/18.
+ */
+public class HelloAction {
+    //Git is a free software
+    private String name;
+    public  String execute(){
+        System.out.println("name ="+name);
+        return Action.SUCCESS;
+    }
+    public String list(){
+        System.out.println("list");
+        return Action.SUCCESS;
+    }
+    public String add(){
+        System.out.println("add");
+        return Action.SUCCESS;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/cn/sxt/action/LongiAction.java
@@ -0,0 +1,37 @@
+package cn.sxt.action;
+
+import com.opensymphony.xwork2.ActionContext;
+
+/**
+ * Created by Administrator on 2016/2/28.
+ */
+public class LongiAction {
+    private String name;
+    private String pwd;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
+
+    public String execute() {
+        if ("zmy".equals(name) && "123".equals(pwd)) {
+            ActionContext.getContext().getSession().put("user",name);
+            return "success";
+        } else {
+            return "login";
+        }
+    }
+}
